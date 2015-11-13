3fe6d38664381389d44eeb5a64bd517bec257713
ssalka <stevensalka925@gmail.com>
2015-11-12 23:12:08 -0800

    in lieu of a UI (for now)

---
+++ inputs.py
@@ -0,0 +1,16 @@
+config = {'client_id': 'YOUR_CLIENT_ID',
+		  'client_secret': 'YOUR_CLIENT_SECRET',
+		  'username': 'soundcloud-user',
+		  'password': 'soundcloud-pw'}
+
+params = 'example.json'
+
+# Create New Stream
+new = True
+
+# Open in Browser
+goto = True
+
+title     = 'Stream Title Goes Here' # For creating new streams
+permalink = 'stream-title-goes-here' # For updating existing streams
+public    = True
